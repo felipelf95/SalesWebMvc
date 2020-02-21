5bc624b23bbf27ea035ec524c9528cdbce9b6a2d
felipe <felipe.p95@outlook.com>
2020-02-21 16:01:58 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,36 @@
 ﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+
+        public Department()
+        {
+        }
+
+        // não incluir atributos do tipo collection...
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void Addseller (Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,32 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+using System.Collections.Generic;
+
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status  { get; set; }
+
+        // associação 1 para muitos 1 vendedor possui 1 ou mais vendas...
+        public Seller Seller { get; set; }
+
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
