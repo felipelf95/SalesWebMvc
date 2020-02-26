b1812c5104117e1259ba63aab62c099206b9120c
felipe <felipe.p95@outlook.com>
2020-02-26 15:00:12 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -17,5 +17,19 @@ namespace SalesWebMvc.Models
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        // page to create a new seller
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // insert a new seller on database...
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
